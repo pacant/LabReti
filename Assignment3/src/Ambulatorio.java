9257b676c35889cd01ba733a17217061277bc242
pacant <a.pace97@outlook.com>
2019-10-18 13:11:31 +0200

    Finito

--- Assignment3/src/Ambulatorio.java
+++ Assignment3/src/Ambulatorio.java
@@ -2,5 +2,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class Ambulatorio {
-    static public boolean [] medici=new boolean[9];
+    static public boolean [] medici=new boolean[10]; //un array contenente flag che indicano se un medico è occupato o meno.
+    static public boolean full=false;  // indica se ci sono codici rossi in attesa
+    static public int redwaiting=0;
 }
