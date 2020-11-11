975c3bf8db9f776a1b973b099d7372e7c23e23b4
pacant <a.pace97@outlook.com>
2020-11-11 17:06:35 +0100

    Piccoli aggiustamenti

---
+++ Assignment 7/src/MainClass.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class MainClass {
+    public static void main(String [] args){
+        int port=6789;
+        try( ServerSocket serverSocket = new ServerSocket(port);){
+            System.out.println("Server listening on port " + port);
+
+            while(true){
+                Server server= new Server(serverSocket.accept());  // un thread per ogni collegamento da parte di un client
+                Thread threadServer= new Thread(server);
+                threadServer.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
